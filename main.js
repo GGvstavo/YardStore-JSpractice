6fee5a40eb8b5c54202ffe307730415105edde08
Gustavo Gimenez <gustavofgimenez.com>
2022-07-28 18:54:39 -0300

    Agrego lo visto en clase 8 para mostrar/ocultar menú desplegable en mobile

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const menuDesktop = document.querySelector('.inactive')
 const navEmail = document.querySelector('.navbar-email');
+const menuHamburgerIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 navEmail.addEventListener('click', () => {
     menuDesktop.classList.toggle('inactive');
+});
+
+menuHamburgerIcon.addEventListener('click', () => {
+    mobileMenu.classList.toggle('inactive');
 });
