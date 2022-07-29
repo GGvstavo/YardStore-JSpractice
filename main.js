85b0d1927241045dfd8d6decc9fca842c96c1a6a
Gustavo Gimenez <gustavofgimenez.com>
2022-07-29 12:32:49 -0300

    Dejo funcionando la navbar con su funcionalidad dada por JS

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-const menuDesktop = document.querySelector('.inactive')
+const menuDesktop = document.querySelector('.desktop-menu')
 const navEmail = document.querySelector('.navbar-email');
 const menuHamburgerIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const myOrderList = document.querySelector('.product-detail');
 
-
+// Link to display profile options
 navEmail.addEventListener('click', () => {
+    myOrderList.classList.add('inactive');
     menuDesktop.classList.toggle('inactive');
 });
 
+// Hamburger menu icon to display menu options
 menuHamburgerIcon.addEventListener('click', () => {
+    myOrderList.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+});
+
+// Cart icon to display order
+shoppingCartIcon.addEventListener('click', () => {
+    mobileMenu.classList.add('inactive');
+    menuDesktop.classList.add('inactive');
+    myOrderList.classList.toggle('inactive');
 });
